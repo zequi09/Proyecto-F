7d09b61d343df68780c42217397115d0aa11db60
zequi09 <ezequiel_mantilla@hotmail.com>
2021-08-21 13:28:27 -0500

    Prueba dos

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const http = require('http')
 
-server.on('error', (err) => {
+const port = 41244
+
+const server  = http.createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Prueba pf')
+})
+
+server.listen(port, () => [
+    console.log(`Server running on port: ${port}`)
+])
+
+
+
+const dgram = require('dgram');
+const serverudp = dgram.createSocket('udp4');
+
+serverudp.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
+serverudp.on('message', (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
-server.on('listening', () => {
+serverudp.on('listening', () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(41244);
+serverudp.bind(41244);
